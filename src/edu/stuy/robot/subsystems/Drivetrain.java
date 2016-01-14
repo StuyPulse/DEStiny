53094c2fdb0a8a592f751f30eb255ec83d23e0e5
adrisj7 <adrisj7@gmail.com>
2016-01-13 19:31:17 -0500

    fixed commit

---
+++ src/edu/stuy/robot/subsystems/Drivetrain.java
@@ -0,0 +1,37 @@
+package edu.stuy.robot.subsystems;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import static edu.stuy.robot.RobotMap.*;
+
+/**
+ *
+ */
+public class Drivetrain extends Subsystem {
+    
+	private CANTalon leftFrontMotor;
+	private CANTalon rightFrontMotor;
+	private CANTalon leftRearMotor;
+	private CANTalon rightRearMotor;
+	private RobotDrive robotDrive;
+	
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	public Drivetrain() {
+		leftFrontMotor = new CANTalon(FRONT_LEFT_MOTOR);
+		rightFrontMotor = new CANTalon(FRONT_RIGHT_MOTOR);
+		leftRearMotor = new CANTalon(REAR_LEFT_MOTOR);
+		rightRearMotor = new CANTalon(REAR_RIGHT_MOTOR);
+		robotDrive = new RobotDrive(leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor);
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	
+    }
+    public void tankDrive(double left, double right) {
+    	robotDrive.tankDrive(left, right);
+    }
+}
+
