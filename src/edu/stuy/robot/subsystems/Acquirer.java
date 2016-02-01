fd25fb1d0feaf865cf9c0885db07d7acfb734619
huiminthehuman <huiminwu24@gmail.com>
2016-02-01 18:16:50 -0500

    Added potentiometer for acquirer

--- src/edu/stuy/robot/subsystems/Acquirer.java
+++ src/edu/stuy/robot/subsystems/Acquirer.java
@@ -1,21 +1,28 @@
 package edu.stuy.robot.subsystems;
 
 import static edu.stuy.robot.RobotMap.ACQUIRER_MOTOR_CHANNEL;
+import static edu.stuy.robot.RobotMap.ACQUIRER_POTENTIOMETER_CHANNEL;
+
 import edu.stuy.robot.commands.AcquirerStopCommand;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 /**
  *
  */
 public class Acquirer extends Subsystem {
 
 	private CANTalon acquirerMotor;
+	private Potentiometer potentiometer;
+	public String outString = "";
 	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
 	public Acquirer() {
 		acquirerMotor = new CANTalon(ACQUIRER_MOTOR_CHANNEL);
+		potentiometer = new AnalogPotentiometer(ACQUIRER_POTENTIOMETER_CHANNEL, 300, 0);
 	}
 
     public void initDefaultCommand() {
@@ -34,5 +41,13 @@ public class Acquirer extends Subsystem {
     public void deacquire() {
     	acquirerMotor.set(-1.0);
     }
-}
 
+    public double getVoltage() {
+    	return potentiometer.get();
+    }
+
+	public double convertVoltage() {
+		double x = getVoltage();
+		return (x - 18) * 15;
+	}
+}
