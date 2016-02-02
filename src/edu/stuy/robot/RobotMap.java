cf976b95d4b39fad1d8bc2d13961b6ca43504f5f
lucasfarachcolton <lucasfc@mac.com>
2016-02-01 19:12:36 -0500

    updated encoder

--- src/edu/stuy/robot/RobotMap.java
+++ src/edu/stuy/robot/RobotMap.java
@@ -16,7 +16,7 @@ public interface RobotMap {
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
 	int FRONT_LEFT_MOTOR_CHANNEL = 2;
-	int FRONT_RIGHT_MOTOR_CHANNEL = 2;
+	int FRONT_RIGHT_MOTOR_CHANNEL = 1;
 	int REAR_LEFT_MOTOR_CHANNEL = 2;
 	int REAR_RIGHT_MOTOR_CHANNEL = 2;
 	int DRIVER_GAMEPAD = 0;
@@ -24,8 +24,9 @@ public interface RobotMap {
 	int SHOOTER_MOTOR_CHANNEL = 2;
 	int FEEDER_MOTOR_CHANNEL = 2;
 	int ACQUIRER_MOTOR_CHANNEL = 2;
-	int ACQUIRER_POTENTIOMETER_CHANNEL = 2;
 	int PISTON_CHANNEL = 2;
 	int DROPDOWN_MOTOR_CHANNEL = 2;
 	int DRIVETRAIN_GYRO_CHANNEL = 1;
+	int WHEEL_DIAMETER = 4;
+	int ENCODER_CHANNEL = 68;
 }
--- src/edu/stuy/robot/subsystems/Shooter.java
+++ src/edu/stuy/robot/subsystems/Shooter.java
@@ -9,9 +9,13 @@ import static edu.stuy.robot.RobotMap.*;
 public class Shooter extends Subsystem {
 
 	private CANTalon shooterMotor;
-
+    private Encoder enc;
+    private double timeBefore;
+    private double timeNow;
+    
 	public Shooter() {
 		shooterMotor = new CANTalon(SHOOTER_MOTOR_CHANNEL);
+		enc = new Encoder(ENCODER_CHANNEL);
 	}
 
 	public void stop() {
@@ -19,15 +23,20 @@ public class Shooter extends Subsystem {
 	}
 
 	public void setSpeed() {
+	    encoderBefore = enc.get();
+	    timeBefore = Timer.getFPGATimestamp();
 		shooterMotor.set(convertAngularSpeedtoToMotorSpeed(calculateSpeed()));
 	}
 
 	private double calculateSpeed() {
-		return 0.0;
+	     int difference = enc.get() - encoderBefore;
+	     double timeDif = Timer.getFPGATimestamp() - timeBefore;
+	     return WHEEL_DIAMETER * Math.PI * difference / timedif;
 	}
 
 	private double convertAngularSpeedtoToMotorSpeed(double angularSpeed) {
-		return 0.0;
+		return angularSpeed;
+		// Need To Do This. This is not done. DONT THINK THAT THIS IS DONE JUST BECAUSE THERE IS CODE HERE
 	}
 
 	public void initDefaultCommand() {
