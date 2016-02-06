6df07354ac9f395fece90869e955b0416925807d
adrisj7 <adrisj7@gmail.com>
2016-02-06 12:18:38 -0500

    Reformat Sonar

--- src/edu/stuy/robot/subsystems/Sonar.java
+++ src/edu/stuy/robot/subsystems/Sonar.java
@@ -6,28 +6,21 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Sonar extends Subsystem {
     public Sonar(String s) {
-        // data = s;
-        // Put the freakin sonar class in here pls
+        // Put the sonar class in here pls
     }
 
-    // string is L1234R1234
-    // private String data;
-    private double[] parser(String s) { // returns in inches
+    // format of raw data is string "L####R####" (like "L1234R1234")
+    private double[] parse(String rawData) { // returns in inches
         try {
-            String noL = s.substring(1);
-            String[] parts = noL.split("R");
+            String dataWithoutL = rawData.substring(1);
+            String[] parts = dataWithoutL.split("R");
             double left = Double.parseDouble(parts[0]);
             double right = Double.parseDouble(parts[1]);
-            double[] values = new double[] { left, right };
-            return values;
+            double[] distances = new double[] { left, right };
+            return distances;
         } catch (Exception e) {
             return null;
         }
-    }
-
-    public static void main(String... args) {
-        Sonar s = new Sonar("asdf");
-        System.out.println(s.sideToTurn(new double[] { 3, 3 }));
     }
 
     /**
@@ -36,34 +29,37 @@ public class Sonar extends Subsystem {
      * robot parallel
      * 
      * <pre>
-     * tan(x) = opposite/adjacent 
-     * adjacent = the distance between the two sensors 
-     * opposite = the difference in distances
+     *  tan(x) = opposite/adjacent 
+     *  adjacent = the distance between the two sensors 
+     *  opposite = the difference in distances
      * 
      * 
-     *  |\ 
-     *  | \ <- the wall 
-     *  |  \ 
-     *  |   \ 
-     *  |    \ 
-     *d1|------ also the angle 				0 = sensors looking ^ direction 
-     *  |  d2| \ 
-     *  |    |  \ 
-     *  0----0 - - angle we want 
-     *    ^ 
-     *    | 
-     * distance between sonar
+     *   |\ 
+     *   | \ <- the wall 
+     *   |  \ 
+     *   |   \ 
+     *   |    \ 
+     * d1|------ also the angle 				0 = sensors looking ^ direction 
+     *   |  d2| \ 
+     *   |    |  \ 
+     *   0----0 - - angle we want 
+     *     ^ 
+     *     | 
+     *  distance between sonar
      * </pre>
      *
      */
-    private boolean isParallel(double[] d) {
-        return angleFinder(d) < ERROR_MARGIN_SONAR;
+    private boolean isParallel(double[] distances) {
+    	return angleFinder(distances) < ERROR_MARGIN_SONAR;
     }
 
-    /*
-     * left = -1 right = 1 parallel = 0
+    /**
+     * <pre>
+     * 	Returns one of three integers:
+     *  left = -1 right = 1 parallel = 0
+     * </pre>
      */
-    private int sideToTurn(double[] d) {
+    private int getSideToTurn(double[] d) {
         if (isParallel(d)) {
             return 0;
         } else if (d[0] < d[1]) {
@@ -73,8 +69,8 @@ public class Sonar extends Subsystem {
         }
     }
 
-    private double angleFinder(double[] d) { // gets input in inches
-        double opposite = Math.abs(d[0] - d[1]);
+    private double angleFinder(double[] distances) { // gets input in inches
+        double opposite = Math.abs(distances[0] - distances[1]);
         double adjacent = DISTANCE_BETWEEN_SONAR;
         return Math.toDegrees(Math.atan(opposite / adjacent));
     }
