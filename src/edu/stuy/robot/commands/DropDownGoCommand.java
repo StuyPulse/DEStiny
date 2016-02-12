d3c4f1d15e4f5237e2677201eb6fe00d0ae9ef52
nicoleshin <shnnicole12321@yahoo.com>
2016-02-12 18:28:37 -0500

    Add timer reset for ramping for dropdown

--- src/edu/stuy/robot/commands/DropDownGoCommand.java
+++ src/edu/stuy/robot/commands/DropDownGoCommand.java
@@ -1,10 +1,12 @@
 package edu.stuy.robot.commands;
+
 import static java.lang.Math.PI;
 import static java.lang.Math.sin;
 
 import edu.stuy.robot.Robot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
+
 /**
  *
  */
@@ -15,47 +17,63 @@ public class DropDownGoCommand extends Command {
 	double startTime;
 	double currentTime;
 
-    public DropDownGoCommand() {
-        // Use requires() here to declare subsystem dependencies
-    	requires(Robot.dropdown);
-    }
+	public DropDownGoCommand() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.dropdown);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
+	// Called just before this Command runs the first time
+	protected void initialize() {
 		startTime = Timer.getFPGATimestamp();
-    }
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-        calculateTime();
-        accelerate();
-    	liftamount = Robot.oi.operatorGamepad.getLeftY() * speedFactor;
-    	Robot.dropdown.go(liftamount);
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (Math.abs(Robot.oi.operatorGamepad.getRightY()) < .1) {
+			initialize();
+		}
+		calculateTime();
+		ramping();
+		liftamount = Robot.oi.operatorGamepad.getLeftY() * speedFactor;
+		Robot.dropdown.go(liftamount);
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 
-	private void calculateTime () {
+	private void calculateTime() {
 		currentTime = Timer.getFPGATimestamp() - startTime;
 	}
 
-	private void accelerate() {
+	private void ramping() {
+		double x = 2 * PI * currentTime - PI / 2;
+		double y = 2 * PI * currentTime + PI / 2;
+		// This is to make sure that the value inside sine does not exceed 90
+		// degrees
+		// The print line that confirms whether or not reseting the timer works
 		if (currentTime <= .5) {
-			speedFactor = .25*sin(2*PI*currentTime-PI/2)+.25;
+			if (x > PI) {
+				x = PI;
+				System.out.println("ABORT");
+			}
+			speedFactor = .25 * sin(x) + .25;
 		} else if (.5 < currentTime && currentTime <= 1) {
-			speedFactor = .25*sin(2*PI*currentTime+PI/2)+.75;
+			if (y > PI) {
+				y = PI;
+				System.out.println("ABORT");
+			}
+			speedFactor = .25 * sin(y) + .75;
 		}
 	}
 }
