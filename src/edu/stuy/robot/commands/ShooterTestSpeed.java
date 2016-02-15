60b77ef459013a630aff7bc97e20bf770b960505
nicoleshin <shnnicole12321@yahoo.com>
2016-02-15 16:18:54 -0500

    Get value from SmartDashboard in execute in ShooterTestSpeed

--- src/edu/stuy/robot/commands/ShooterTestSpeed.java
+++ src/edu/stuy/robot/commands/ShooterTestSpeed.java
@@ -9,34 +9,34 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class ShooterTestSpeed extends Command {
 
-	private double _speed;
+	private double speed;
 
-    public ShooterTestSpeed() {
-        // Use requires() here to declare subsystem dependencies
-        requires(Robot.shooter);
-        _speed = SmartDashboard.getNumber("Shooter Speed");
-    }
+	public ShooterTestSpeed() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.shooter);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	Robot.shooter.setSpeed(_speed);
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		speed = SmartDashboard.getNumber("Shooter Speed");
+		Robot.shooter.setSpeed(speed);
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
