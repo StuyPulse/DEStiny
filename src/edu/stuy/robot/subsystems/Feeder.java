aea14617852fd475e3c2327df0041ba1dbc45d3f
nicoleshin <shnnicole12321@yahoo.com>
2016-02-15 17:58:30 -0500

    Add reverseFeed command

--- src/edu/stuy/robot/subsystems/Feeder.java
+++ src/edu/stuy/robot/subsystems/Feeder.java
@@ -4,29 +4,34 @@ import edu.stuy.robot.commands.FeederStopCommand;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import static edu.stuy.robot.RobotMap.*;
+
 /**
  *
  */
 public class Feeder extends Subsystem {
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-    private CANTalon feederMotor;
-    
-    public Feeder() {
-    	feederMotor = new CANTalon(FEEDER_MOTOR_CHANNEL);
-    }
-  
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    	setDefaultCommand(new FeederStopCommand());
-    }
-    
-    public void stop() {
-    	feederMotor.set(0.0);
-    }
-    
-    public void feed() {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	private CANTalon feederMotor;
+
+	public Feeder() {
+		feederMotor = new CANTalon(FEEDER_MOTOR_CHANNEL);
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+		setDefaultCommand(new FeederStopCommand());
+	}
+
+	public void stop() {
+		feederMotor.set(0.0);
+	}
+
+	public void feed() {
 		feederMotor.set(1.0);
+	}
+
+	public void reverseFeed() {
+		feederMotor.set(-1.0);
 	}
 }
