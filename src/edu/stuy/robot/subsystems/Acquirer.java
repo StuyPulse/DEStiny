fc73c6bfb1cf9c8e762516a4a705d4b7314c3a99
elc1798 <subliminalmau5@gmail.com>
2016-02-16 20:41:12 -0500

    Move potentiometer to dropdown and re-tune it

--- src/edu/stuy/robot/subsystems/Acquirer.java
+++ src/edu/stuy/robot/subsystems/Acquirer.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 public class Acquirer extends Subsystem {
 
 	private CANTalon acquirerMotor;
-	private Potentiometer potentiometer;
 	private String outString = "";
 
 	// Put methods for controlling this subsystem
@@ -22,14 +21,9 @@ public class Acquirer extends Subsystem {
 
 	public Acquirer() {
 		acquirerMotor = new CANTalon(ACQUIRER_MOTOR_CHANNEL);
-		potentiometer = new AnalogPotentiometer(ACQUIRER_POTENTIOMETER_CHANNEL,
-				300, 0);
 	}
 
 	// Used for auton
-	public void move(double speed) {
-		acquirerMotor.set(speed);
-	}
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
@@ -46,15 +40,6 @@ public class Acquirer extends Subsystem {
 
 	public void deacquire() {
 		acquirerMotor.set(-1.0);
-	}
-
-	public double getVoltage() {
-		return potentiometer.get();
-	}
-
-	public double getAngle() {
-		double x = getVoltage();
-		return (x - 18) * 15;
 	}
 
 	public void lowerAcquirerToDrivingPosition() {
--- src/edu/stuy/robot/subsystems/DropDown.java
+++ src/edu/stuy/robot/subsystems/DropDown.java
@@ -1,12 +1,17 @@
 package edu.stuy.robot.subsystems;
 
+import static edu.stuy.robot.RobotMap.ACQUIRER_POTENTIOMETER_CHANNEL;
+import static edu.stuy.robot.RobotMap.CONVERSION_FACTOR;
 import static edu.stuy.robot.RobotMap.DROPDOWN_MOTOR_CHANNEL;
-import static edu.stuy.robot.RobotMap.LIMIT_SWITCH_CHANNEL;
+import static edu.stuy.robot.RobotMap.INITIAL_VOLTAGE;
+
 import edu.stuy.robot.commands.DropDownGoCommand;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 //import edu.stuy.robot.commands.DropDownGoCommand;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 /**
  *
@@ -15,12 +20,16 @@ public class DropDown extends Subsystem {
 
 	private CANTalon dropDownMotor;
 	private DigitalInput limitSwitch;
+	private Potentiometer potentiometer;
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 
 	public DropDown() {
 		dropDownMotor = new CANTalon(DROPDOWN_MOTOR_CHANNEL);
-		limitSwitch = new DigitalInput(LIMIT_SWITCH_CHANNEL);
+		dropDownMotor.setInverted(true);
+		// limitSwitch = new DigitalInput(LIMIT_SWITCH_CHANNEL);
+		potentiometer = new AnalogPotentiometer(ACQUIRER_POTENTIOMETER_CHANNEL,
+				300, 0);
 	}
 
 	public void initDefaultCommand() {
@@ -32,11 +41,24 @@ public class DropDown extends Subsystem {
 		if (limitSwitch.get() && speed < 0.0) {
 			stop();
 		} else {
-			dropDownMotor.set(speed);
+		dropDownMotor.set(speed);
 		}
-    }
+	}
 
 	public void stop() {
 		dropDownMotor.set(0.0);
+	}
+
+	public double getVoltage() {
+		return potentiometer.get();
+	}
+
+	public double getAngle() {
+		double x = getVoltage();
+		return (x - INITIAL_VOLTAGE) * CONVERSION_FACTOR;
+	}
+
+	public void move(double speed) {
+		dropDownMotor.set(speed);
 	}
 }
