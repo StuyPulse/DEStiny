0f1cfb2fec9fd99569aff1380fcb59c67060c611
adrisj7 <adrisj7@gmail.com>
2016-02-19 14:49:14 -0500

    Remove jerking from EfficientRamper (Should be about 100 percent silky smooth)

--- src/edu/stuy/util/EfficientRamper.java
+++ src/edu/stuy/util/EfficientRamper.java
@@ -7,6 +7,9 @@ public class EfficientRamper extends Ramper{
 	private double velocity;
 	// For increasing/accelerating.
 
+	private double lastValue;
+	// For updating velocity and preventing jerking
+
 	private final double STOP_THRESHHOLD = 0.3;
 	private final double END_DAMPENING = 0.1;
 	private final double START_DAMPENING = 0.001;
@@ -15,16 +18,19 @@ public class EfficientRamper extends Ramper{
 		target = 0.0;
 		value = 0.0;
 		velocity = 0;
+		lastValue = 0;
 	}
 
 	public void update() {
 		if (Math.abs(target - value) < STOP_THRESHHOLD) {
 			value += (target - value) * END_DAMPENING;
-			velocity = 0;
+			velocity = value - lastValue;
+			// Such that when switching, it will be smooth.
 		} else {
 			velocity += START_DAMPENING * Math.signum(target - value);
 			value += velocity;
 		}
+		lastValue = value;
 	}
 
 	public void setTarget(double target) {
