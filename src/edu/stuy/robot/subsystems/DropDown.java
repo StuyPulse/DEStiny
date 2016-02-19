2cb4783787c19c17745222a2afb039c0991cce08
nicoleshin <shnnicole12321@yahoo.com>
2016-02-18 20:22:33 -0500

    Remove limit switch from dropdown

--- src/edu/stuy/robot/subsystems/DropDown.java
+++ src/edu/stuy/robot/subsystems/DropDown.java
@@ -1,15 +1,10 @@
 package edu.stuy.robot.subsystems;
 
 import static edu.stuy.robot.RobotMap.ACQUIRER_POTENTIOMETER_CHANNEL;
-import static edu.stuy.robot.RobotMap.CONVERSION_FACTOR;
 import static edu.stuy.robot.RobotMap.DROPDOWN_MOTOR_CHANNEL;
-import static edu.stuy.robot.RobotMap.INITIAL_VOLTAGE;
-import static edu.stuy.robot.RobotMap.LIMIT_SWITCH_CHANNEL;
-
 import edu.stuy.robot.commands.DropDownGoCommand;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -20,7 +15,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class DropDown extends Subsystem {
 
 	private CANTalon dropDownMotor;
-	private DigitalInput limitSwitch;
 	private Potentiometer potentiometer;
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
@@ -28,7 +22,6 @@ public class DropDown extends Subsystem {
 	public DropDown() {
 		dropDownMotor = new CANTalon(DROPDOWN_MOTOR_CHANNEL);
 		dropDownMotor.setInverted(true);
-		limitSwitch = new DigitalInput(LIMIT_SWITCH_CHANNEL);
 		potentiometer = new AnalogPotentiometer(ACQUIRER_POTENTIOMETER_CHANNEL, 300, 0);
 	}
 
@@ -38,11 +31,7 @@ public class DropDown extends Subsystem {
 	}
 
 	public void go(double speed) {
-		if (limitSwitch.get() && speed < 0.0) {
-			stop();
-		} else {
-			dropDownMotor.set(speed);
-		}
+		dropDownMotor.set(speed);
 	}
 
 	public void stop() {
