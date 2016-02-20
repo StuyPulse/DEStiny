a2804ab434ade81d23151e3566d8ee9e84e0b67c
Wilson Berkow <wilsonberkow@gmail.com>
2016-02-20 11:27:46 -0500

    Fix bugs introduced with auto-aiming override code, and reformat class to fit style guide

--- src/edu/stuy/robot/commands/SetupforShotCommand.java
+++ src/edu/stuy/robot/commands/SetupforShotCommand.java
@@ -9,66 +9,68 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class SetupforShotCommand extends Command {
-	private TegraDataReader reader;
-	private double[] currentReading;
-	private boolean goalInFrame;
-	private boolean stopAiming = false;
 
-	private static double pxOffsetToDegrees(double px) {
-		return CAMERA_VIEWING_ANGLE_X * px / CAMERA_FRAME_PX_WIDTH;
-	}
+    private TegraDataReader reader;
+    private double[] currentReading;
+    private boolean goalInFrame;
+    private boolean forceStopped = false;
 
-	public SetupforShotCommand() {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
-		requires(Robot.shooter);
-		requires(Robot.drivetrain);
-	}
+    private static double pxOffsetToDegrees(double px) {
+        return CAMERA_VIEWING_ANGLE_X * px / CAMERA_FRAME_PX_WIDTH;
+    }
 
-	// Called just before this Command runs the first time
-	protected void initialize() {
-		// Robot.shooter.setSpeedTesting(0.7);
-		reader = new TegraDataReader();
-		goalInFrame = true; // Assume it is there until we see otherwise
-	}
+    public SetupforShotCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(Robot.drivetrain);
+    }
 
-	// Called repeatedly when this Command is scheduled to run
-	protected void execute() {
-		if (!stopAiming) {
-			if (currentReading == null) {
-			    goalInFrame = false;
-			    return;
-			}
-			currentReading = reader.readVector();
-			double degsOff = pxOffsetToDegrees(currentReading[0]);
-			// TODO: Do real math, write non-wack calculation of rightWheelSpeed
-			double rightWheelSpeed = -degsOff / (CAMERA_FRAME_PX_WIDTH / 2);
-			Robot.drivetrain.tankDrive(-rightWheelSpeed, rightWheelSpeed);
-		}
-		if (Robot.oi.operatorGamepad.getStartButton().get()) {
-			stopAiming = true;
-		}
-	}
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        // Robot.shooter.setSpeedTesting(0.7);
+        reader = new TegraDataReader();
+        goalInFrame = true; // Assume it is there until we see otherwise
+    }
 
-	// Make this return true when this Command no longer needs to run execute()
-	protected boolean isFinished() {
-	    if (!goalInFrame) {
-	        return true;
-	    }
-		double degsOff = pxOffsetToDegrees(currentReading[0]);
-		return Math.abs(degsOff) < MAX_DEGREES_OFF_AUTO_AIMING;
-	}
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        if (Robot.oi.operatorGamepad.getStartButton().get()) {
+            forceStopped = true;
+        }
+        if (!forceStopped) {
+            currentReading = reader.readVector();
+            if (currentReading == null) {
+                goalInFrame = false;
+                return;
+            }
+            double degsOff = pxOffsetToDegrees(currentReading[0]);
+            // TODO: Do real math; write non-wack calculation of rightWheelSpeed
+            // Set rightWheelSpeed to the ratio of how far off it
+            // is to how far it could possibly be off
+            double rightWheelSpeed = -degsOff / (CAMERA_FRAME_PX_WIDTH / 2);
+            Robot.drivetrain.tankDrive(-rightWheelSpeed, rightWheelSpeed);
+        }
+    }
 
-	// Called once after isFinished returns true
-	protected void end() {
-		reader.closePort();
-		reader = null;
-		// TODO: Handle goalInFrame being false (e.g. with the LEDs)
-	}
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        if (!goalInFrame || forceStopped) {
+            return true;
+        }
+        double degsOff = pxOffsetToDegrees(currentReading[0]);
+        return Math.abs(degsOff) < MAX_DEGREES_OFF_AUTO_AIMING;
+    }
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	protected void interrupted() {
-		reader.closePort();
-	}
+    // Called once after isFinished returns true
+    protected void end() {
+        reader.closePort();
+        reader = null;
+        // TODO: Handle goalInFrame being false (e.g. with the LEDs)
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        reader.closePort();
+    }
 }
