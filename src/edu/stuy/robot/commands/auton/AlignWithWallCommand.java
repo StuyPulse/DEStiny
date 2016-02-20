507270e374564915a008a2314388ca48417f05f9
adrisj7 <adrisj7@gmail.com>
2016-02-20 11:17:40 -0500

    Fixed redundancy with something slightly less redundant

--- src/edu/stuy/robot/commands/auton/AlignWithWallCommand.java
+++ src/edu/stuy/robot/commands/auton/AlignWithWallCommand.java
@@ -20,7 +20,8 @@ public class AlignWithWallCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        direction = Robot.sonar.getSideToTurn();
+        // Semi-redundant initialization.
+        direction = 1;
     }
 
     // Called repeatedly when this Command is scheduled to run
