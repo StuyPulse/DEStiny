719123e34eea3281fa9e59a77674dd123e127cfd
elc1798 <subliminalmau5@gmail.com>
2016-03-12 09:10:49 -0500

    Create failsafe in DropDownMoveToAngleCommand so in case potentiometer comes out again, it will not move backwards

--- src/edu/stuy/robot/commands/auton/DropDownMoveToAngleCommand.java
+++ src/edu/stuy/robot/commands/auton/DropDownMoveToAngleCommand.java
@@ -1,42 +1,61 @@
 package edu.stuy.robot.commands.auton;
 
 import edu.stuy.robot.Robot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class DropDownMoveToAngleCommand extends Command{
-	
-	private double desiredAngle;
-	
-	public DropDownMoveToAngleCommand(double angle) {
-		desiredAngle = angle;
-		requires(Robot.dropdown);
-	}
-	
-	@Override
-	protected void initialize() {
-	}
+public class DropDownMoveToAngleCommand extends Command {
 
-	@Override
-	protected void execute() {
-		if (Robot.dropdown.getAngle() < desiredAngle) {
-			Robot.dropdown.move(-0.4);
-		} else {
-			Robot.dropdown.move(0.55);
-		}
-	}
+    private double desiredAngle;
+    private double timeStart; // If potentiometer is disconnected, readings will
+    private double initialAngle; // constantly read -91.xx. It will also be
+                                 // approx.
+                                 // static. If we run the motors for more than
+                                 // 2s
+                                 // and it still hasn't changed, stop running.
 
-	@Override
-	protected boolean isFinished() {
-		return Math.abs(Robot.dropdown.getAngle() - desiredAngle) < 4.0;
-	}
+    public DropDownMoveToAngleCommand(double angle) {
+        desiredAngle = angle;
+        requires(Robot.dropdown);
+    }
 
-	@Override
-	protected void end() {
-	    Robot.dropdown.currentAngle = desiredAngle;
-	    Robot.dropdown.move(0.0);
-	}
+    @Override
+    protected void initialize() {
+        timeStart = Timer.getFPGATimestamp();
+        initialAngle = Robot.dropdown.getAngle();
+    }
 
-	@Override
-	protected void interrupted() {
-	}
+    @Override
+    protected void execute() {
+        if (Robot.dropdown.getAngle() < desiredAngle) {
+            Robot.dropdown.move(-0.4);
+        } else {
+            Robot.dropdown.move(0.55);
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return Robot.dropdown.getAngle() < -20.0 ||
+                !is420Working() ||
+                Math.abs(Robot.dropdown.getAngle() - desiredAngle) < 4.0;
+    }
+
+    @Override
+    protected void end() {
+        Robot.dropdown.currentAngle = desiredAngle;
+        Robot.dropdown.move(0.0);
+    }
+
+    @Override
+    protected void interrupted() {
+    }
+
+    private boolean is420Working() {
+        if (Timer.getFPGATimestamp() - timeStart < 1.0) {
+            return true;
+        } else {
+            return Math.abs(Robot.dropdown.getAngle() - initialAngle) > 2.0;
+        }
+    }
 }
