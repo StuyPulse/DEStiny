ff82299ef0a1dd6f79ca15ef1312238383b0fd2d
Wilson Berkow <wilsonberkow@gmail.com>
2016-03-13 09:07:22 -0400

    Turn off signal light in end() of ShooterStopCommand

--- src/edu/stuy/robot/commands/ShooterStopCommand.java
+++ src/edu/stuy/robot/commands/ShooterStopCommand.java
@@ -30,6 +30,7 @@ public class ShooterStopCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+        Robot.redSignalLight.setOff();
     }
 
     // Called when another command which requires one or more of the same
