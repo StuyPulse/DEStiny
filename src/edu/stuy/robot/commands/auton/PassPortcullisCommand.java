57b23931815c80a75c51fc344a24f7fac357104d
elc1798 <subliminalmau5@gmail.com>
2016-03-31 15:33:31 -0400

    Roughly update portcullis command

--- src/edu/stuy/robot/commands/auton/PassPortcullisCommand.java
+++ src/edu/stuy/robot/commands/auton/PassPortcullisCommand.java
@@ -4,15 +4,21 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class PassPortcullisCommand extends CommandGroup {
 
-	private static final int MAX_DISTANCE_FORWARD_IN_INCHES = 48;
-	private static final int MAX_TIME_FORWARD_IN_SECONDS = 10;
-	private static final double SPEED = 1.0;
+    private static final int INITIAL_DISTANCE_FORWARD_IN_INCHES = 100;
+    private static final int INITIAL_TIME_FORWARD_IN_SECONDS = 10;
+    private static final double INITIAL_SPEED = 0.5;
+    private static final int SECOND_DISTANCE_FORWARD_IN_INCHES = 150;
+    private static final int SECOND_TIME_FORWARD_IN_SECONDS = 10;
+    private static final double SECOND_SPEED = 1.0;
 	
 	
 	public PassPortcullisCommand() {
-		addSequential(new DropDownMoveToAngleCommand(90));
-		addSequential(new DriveForwardCommand(MAX_DISTANCE_FORWARD_IN_INCHES, MAX_TIME_FORWARD_IN_SECONDS, SPEED));
-		addParallel(new DropDownMoveToAngleCommand(45));
-		addSequential(new DriveForwardCommand(MAX_DISTANCE_FORWARD_IN_INCHES, MAX_TIME_FORWARD_IN_SECONDS, SPEED));
+	    // drive forward
+	    // raise arm 45
+	    // forward fast and raise to 70
+	    addSequential(new DriveForwardCommand(INITIAL_DISTANCE_FORWARD_IN_INCHES, INITIAL_TIME_FORWARD_IN_SECONDS, INITIAL_SPEED));
+	    addSequential(new DropDownMoveToAngleCommand(45));
+	    addParallel(new DriveForwardCommand(SECOND_DISTANCE_FORWARD_IN_INCHES, SECOND_TIME_FORWARD_IN_SECONDS, SECOND_SPEED));
+	    addSequential(new DropDownMoveToAngleCommand(70));
 	}
 }
