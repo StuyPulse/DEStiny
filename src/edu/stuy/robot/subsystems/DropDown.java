e91b3505c683d4fb731a4c97069ef22a7d014a83
elc1798 <subliminalmau5@gmail.com>
2016-03-31 12:10:01 -0400

    Adjust potentiometer voltages

--- src/edu/stuy/robot/subsystems/DropDown.java
+++ src/edu/stuy/robot/subsystems/DropDown.java
@@ -48,8 +48,8 @@ public class DropDown extends Subsystem {
 
     public double getAngle() {
         double x = getVoltage();
-        double initialVoltage = 182.0;
-        double finalVoltage = 252.5;
+        double initialVoltage = 212.0;
+        double finalVoltage = 288.0;
         double conversionFactor = 90.0 / (finalVoltage - initialVoltage);
         return (x - initialVoltage) * conversionFactor;
     }
