44e7c498ff6bfec967198a7ebd774dff0d8ac8f2
Wilson Berkow <wilsonberkow@gmail.com>
2016-04-01 09:26:00 -0400

    Add 5sec timeout to RotateToAimCommand and wrap in try catches

--- src/edu/stuy/robot/commands/RotateToAimCommand.java
+++ src/edu/stuy/robot/commands/RotateToAimCommand.java
@@ -18,6 +18,9 @@ public class RotateToAimCommand extends Command {
     private double desiredAngle;
     private boolean priorGearShiftState;
 
+    private static final long timeout = 5000;
+    private long timeStart;
+
     private static double pxOffsetToDegrees(double px) {
         return CAMERA_VIEWING_ANGLE_X * px / CAMERA_FRAME_PX_HEIGHT;
     }
@@ -30,20 +33,26 @@ public class RotateToAimCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        Robot.drivetrain.resetGyro();
-        long start = System.currentTimeMillis();
-        double[] reading = Robot.vision.processImage();
-        System.out.println("Image processing took " + (System.currentTimeMillis() - start) + "ms");
-        if (reading == null) {
-            goalInFrame = false;
-            System.out.println("Reading was NULL");
-        } else {
-            desiredAngle = pxOffsetToDegrees(reading[0]);
-            System.out.println("Reading was: " + Arrays.toString(reading));
-            System.out.println("Desired Angle Delta: " + desiredAngle);;
+        try {
+            Robot.drivetrain.resetGyro();
+            timeStart = System.currentTimeMillis();
+            double[] reading = Robot.vision.processImage();
+            System.out.println("Image processing took " + (System.currentTimeMillis() - timeStart) + "ms");
+            if (reading == null) {
+                goalInFrame = false;
+                System.out.println("Reading was NULL");
+            } else {
+                desiredAngle = pxOffsetToDegrees(reading[0]);
+                System.out.println("Reading was: " + Arrays.toString(reading));
+                System.out.println("Desired Angle Delta: " + desiredAngle);;
+            }
+            priorGearShiftState = Robot.drivetrain.gearUp;
+            Robot.drivetrain.manualGearShift(true);
+        } catch (Exception e) {
+            System.out.println("Error in intialize in rotatetoaim:");
+            e.printStackTrace();
+            forceStopped = true; // abort command
         }
-        priorGearShiftState = Robot.drivetrain.gearUp;
-        Robot.drivetrain.manualGearShift(true);
     }
 
     private double howFarHaveWeCome() {
@@ -55,41 +64,60 @@ public class RotateToAimCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        if (Robot.oi.driverGamepad.getRightButton().get()) {
-            forceStopped = true;
-            return;
-        }
-
-        // Simplicity: double speed = 0.5;
-        double speed = 0.9 - 0.5 * howFarHaveWeCome();
-        // right is negative when turning right
-        if (desiredAngle < 0) {
-            Robot.drivetrain.tankDrive(-speed, speed);
-        } else {
-            Robot.drivetrain.tankDrive(speed, -speed);
+        try {
+            if (Robot.oi.driverGamepad.getRightButton().get()) {
+                forceStopped = true;
+                return;
+            }
+            if (!forceStopped) {
+                // Simplicity: double speed = 0.5;
+                double speed = 0.9 - 0.5 * howFarHaveWeCome();
+                // right is negative when turning right
+                if (desiredAngle < 0) {
+                    Robot.drivetrain.tankDrive(-speed, speed);
+                } else {
+                    Robot.drivetrain.tankDrive(speed, -speed);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error in execute in rotatetoaim:");
+            e.printStackTrace();
+            forceStopped = true; // abort command
         }
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        // When no more can or should be done:
-        if (forceStopped || !goalInFrame) {
-            Robot.cvSignalLight.setOff();
-            return true;
-        }
+        try {
+            // When no more can or should be done:
+            if (forceStopped || !goalInFrame) {
+                Robot.cvSignalLight.setOff();
+                return true;
+            }
 
-        // Judgement of success:
-        double degsOff;
-        if (desiredAngle < 0) {
-            degsOff = Robot.drivetrain.getGyroAngle() - (360 + desiredAngle);
-        } else {
-            degsOff = Robot.drivetrain.getGyroAngle() - desiredAngle;
-        }
-        SmartDashboard.putNumber("CV degrees off", degsOff);
+            // Judgement of success:
+            double degsOff;
+            if (desiredAngle < 0) {
+                degsOff = Robot.drivetrain.getGyroAngle() - (360 + desiredAngle);
+            } else {
+                degsOff = Robot.drivetrain.getGyroAngle() - desiredAngle;
+            }
+            SmartDashboard.putNumber("CV degrees off", degsOff);
 
-        boolean onTarget = Math.abs(degsOff) < MAX_DEGREES_OFF_AUTO_AIMING;
-        Robot.cvSignalLight.set(onTarget);
-        return onTarget;
+            boolean onTarget = Math.abs(degsOff) < MAX_DEGREES_OFF_AUTO_AIMING;
+            Robot.cvSignalLight.set(onTarget);
+
+            if (System.currentTimeMillis() - timeStart > timeout) {
+                System.out.println("RotateToAimCommand timed out after " + timeout + "ms");
+                return true;
+            }
+
+            return onTarget;
+        } catch (Exception e) {
+            System.out.println("Error in isFinished in rotatetoaim:");
+            e.printStackTrace();
+            return true; // abort
+        }
     }
 
     // Called once after isFinished returns true
