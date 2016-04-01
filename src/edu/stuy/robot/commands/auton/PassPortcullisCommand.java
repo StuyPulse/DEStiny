2d3fcbcf5befb5956069d0b3bf7c296177e8924b
elc1798 <subliminalmau5@gmail.com>
2016-04-01 11:32:56 -0400

    Lower timeout for portcullis auton

--- src/edu/stuy/robot/commands/auton/PassPortcullisCommand.java
+++ src/edu/stuy/robot/commands/auton/PassPortcullisCommand.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class PassPortcullisCommand extends CommandGroup {
 
     private static final int INITIAL_DISTANCE_FORWARD_IN_INCHES = 100;
-    private static final int INITIAL_TIME_FORWARD_IN_SECONDS = 8;
+    private static final int INITIAL_TIME_FORWARD_IN_SECONDS = 7;
     private static final double INITIAL_SPEED = 0.5;
     private static final int SECOND_DISTANCE_FORWARD_IN_INCHES = 150;
     private static final int SECOND_TIME_FORWARD_IN_SECONDS = 10;
@@ -17,7 +17,7 @@ public class PassPortcullisCommand extends CommandGroup {
 	    // raise arm 45
 	    // forward fast and raise to 70
 	    addSequential(new DropDownMoveToAngleCommand(10));
-	    addSequential(new DriveForwardCommand(INITIAL_DISTANCE_FORWARD_IN_INCHES, INITIAL_TIME_FORWARD_IN_SECONDS, INITIAL_SPEED));
+	    addSequential(new DriveForwardCommand(INITIAL_DISTANCE_FORWARD_IN_INCHES, INITIAL_TIME_FORWARD_IN_SECONDS, INITIAL_SPEED), INITIAL_TIME_FORWARD_IN_SECONDS);
 	    addParallel(new DriveForwardCommand(SECOND_DISTANCE_FORWARD_IN_INCHES, SECOND_TIME_FORWARD_IN_SECONDS, SECOND_SPEED));
 	    addSequential(new DropDownMoveToAngleCommand(70));
 	}
