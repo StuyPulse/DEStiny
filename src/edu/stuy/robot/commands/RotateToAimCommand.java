235f49500052b534fbabd2f949b301b86166f8db
elc1798 <subliminalmau5@gmail.com>
2016-04-24 19:41:50 -0400

    Remove print lines in CV turning command

--- src/edu/stuy/robot/commands/RotateToAimCommand.java
+++ src/edu/stuy/robot/commands/RotateToAimCommand.java
@@ -20,18 +20,12 @@ public class RotateToAimCommand extends GyroRotationalCommand {
     private double[] cvReading;
 
     protected void setDesiredAngle() {
-        long start = System.currentTimeMillis();
         cvReading = Robot.vision.processImage();
-        System.out.println("Image processing took " + (System.currentTimeMillis() - start) + "ms");
         canProceed = cvReading != null;
         SmartDashboard.putString("cv-reading", Arrays.toString(cvReading));
         if (canProceed) {
             desiredAngle = StuyVision.frameXPxToDegrees(cvReading[0]);
             SmartDashboard.putNumber("cv-angle", desiredAngle);
-            System.out.println("Reading was: " + Arrays.toString(cvReading) + "-----------------------------");
-            System.out.println("Desired Angle Delta: " + desiredAngle);
-        } else {
-            System.out.println("Reading was NULL------------------------------------------------------------");
         }
         SmartDashboard.putBoolean("cv-visible", canProceed);
     }
