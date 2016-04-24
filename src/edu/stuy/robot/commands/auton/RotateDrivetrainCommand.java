ea50782be4dadb9cbc547b144faba8df0359b1ae
elc1798 <subliminalmau5@gmail.com>
2016-04-24 19:43:14 -0400

    Change RotateDrivetrainCommand to extend GyroRotationalCommand

--- src/edu/stuy/robot/commands/auton/RotateDrivetrainCommand.java
+++ src/edu/stuy/robot/commands/auton/RotateDrivetrainCommand.java
@@ -6,73 +6,26 @@ import static edu.stuy.robot.RobotMap.SLOT_ANGLE_TO_GOAL_4;
 import static edu.stuy.robot.RobotMap.SLOT_ANGLE_TO_GOAL_5;
 
 import edu.stuy.robot.Robot;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.stuy.robot.commands.GyroRotationalCommand;
 
 /**
  * Turns robot relative to its current position
  */
-public class RotateDrivetrainCommand extends Command {
+public class RotateDrivetrainCommand extends GyroRotationalCommand {
 
-    private double angle;
-    private boolean angleAtRunTime;
-
-    public RotateDrivetrainCommand(double angle) {
-        // Use requires() here to declare subsystem dependencies
-        requires(Robot.drivetrain);
-        this.angle = angle;
-    }
-
-    public RotateDrivetrainCommand() {
-        requires(Robot.drivetrain);
-        angleAtRunTime = true;
-    }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-        Robot.drivetrain.resetGyro();
-        if (angleAtRunTime) {
-            Integer autonPosition = (Integer) Robot.autonPositionChooser.getSelected();
-            angle = correspondingAngle(autonPosition);
-        }
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-        // right is negative when turning right
-        if (angle < 0) {
-            Robot.drivetrain.tankDrive(-0.5, 0.5);
-        } else {
-            Robot.drivetrain.tankDrive(0.5, -0.5);
-        }
-    }
-
-    private double angleMoved() {
-        return Robot.drivetrain.getGyroAngle() * 1.1;
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return Math.abs(angleMoved() - angle) < 1.0;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
-
-    private double correspondingAngle(Integer position) {
+    @Override
+    protected void setDesiredAngle() {
+        int position = (Integer) Robot.autonPositionChooser.getSelected();
         if (position == 2) {
-            return SLOT_ANGLE_TO_GOAL_2;
+            desiredAngle = SLOT_ANGLE_TO_GOAL_2;
         } else if (position == 3) {
-            return SLOT_ANGLE_TO_GOAL_3;
+            desiredAngle = SLOT_ANGLE_TO_GOAL_3;
         } else if (position == 4) {
-            return SLOT_ANGLE_TO_GOAL_4;
+            desiredAngle = SLOT_ANGLE_TO_GOAL_4;
+        } else if (position == 5) {
+            desiredAngle = SLOT_ANGLE_TO_GOAL_5;
         } else {
-            return SLOT_ANGLE_TO_GOAL_5;
+            desiredAngle = 0.0;
         }
     }
 }
