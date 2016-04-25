a4603666326caf7e1ee74baf144b8c42d55fb5c3
elc1798 <subliminalmau5@gmail.com>
2016-04-24 20:32:47 -0400

    Fix issue with state variable not being set correctly

--- src/edu/stuy/robot/subsystems/Hood.java
+++ src/edu/stuy/robot/subsystems/Hood.java
@@ -9,27 +9,27 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Hood extends Subsystem {
 
-	private Solenoid hoodSolenoid;
-	private boolean up;
+    private Solenoid hoodSolenoid;
+    private boolean up;
 
-	// Put methods for controlling this subsystem
-	// here. Call these from Commands.
-	public Hood() {
-		hoodSolenoid = new Solenoid(0, HOOD_SOLENOID_CHANNEL);
-		up = true;
-	}
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    public Hood() {
+        hoodSolenoid = new Solenoid(0, HOOD_SOLENOID_CHANNEL);
+        up = true;
+    }
 
-	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
-	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
 
-	public void changePosition(boolean x) {
-		hoodSolenoid.set(x);
-	    up = !up;
-	}
+    public void changePosition(boolean isUp) {
+        hoodSolenoid.set(isUp);
+        up = isUp;
+    }
 
-	public boolean getState() {
-		return up;
-	}
+    public boolean getState() {
+        return up;
+    }
 }
