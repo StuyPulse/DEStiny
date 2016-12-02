ad1385daeca3395d1634333cfad98e0c192c2fa2
James Wang <jameswang9909@hotmail.com>
2016-12-01 19:42:58 -0500

    Use a thread for CV
    Doesn't block the drivetrain from moving, and makes movement a lot
    smoother

--- src/edu/stuy/robot/commands/FollowObjectCommand.java
+++ src/edu/stuy/robot/commands/FollowObjectCommand.java
@@ -18,9 +18,10 @@ public class FollowObjectCommand extends AutoMovementCommand {
 
     private boolean done;
     private double[] cvReading;
+    private Thread cvThread;
 
     // max speed is 0.8 motor value
-    private final static double distForMaxSpeed = 5 * 12.0;
+    private final static double distForMaxSpeed = (5 * 12.0) * 5;
 
     public FollowObjectCommand() {
         // Use requires() here to declare subsystem dependencies
@@ -36,18 +37,15 @@ public class FollowObjectCommand extends AutoMovementCommand {
         Robot.drivetrain.resetGyro();
         count = 0;
         done = false;
+        cvThread = new Thread(new CVThread());
+        cvThread.setDaemon(true);
+        cvThread.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         super.execute();
         if (!getForceStopped()) {
-            count++;
-
-            if (count == 5) {
-                count = 0;
-                cvReading = Robot.vision.processImage();
-            }
 
             if (cvReading == null) {
                 // Couldn't find the object
@@ -57,15 +55,14 @@ public class FollowObjectCommand extends AutoMovementCommand {
             }
 
             // Adjust the angle first, then the distance
-            angle = StuyVision.frameXPxToDegrees(cvReading[0]);
+            // angle = StuyVision.frameXPxToDegrees(cvReading[0]);
             if (Math.abs(angle) > 5.0) {
                 System.out.println("Rotating");
+                Robot.drivetrain.resetGyro();
                 rotate();
 
-                // Reset the encoders in case we need to re-adjust our distance later
-                Robot.drivetrain.resetEncoders();
             } else {
-                distance = StuyVision.findBotDistanceToGoal(cvReading[1]);
+                // distance = StuyVision.findBotDistanceToGoal(cvReading[1]);
                 if (Math.abs(distance) <= 80) {
                     // We're in range
                     System.out.println("In range");
@@ -74,34 +71,36 @@ public class FollowObjectCommand extends AutoMovementCommand {
                 System.out.println("Adjusting distance");
 
                 // We need to adjust our distance
+                Robot.drivetrain.resetEncoders();
                 adjustDistance();
 
-                // Reset the gyro in case we need to re-adjust our angle later
-                Robot.drivetrain.resetGyro();
             }
         }
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return getForceStopped() || done;
+        return false;
+        // return getForceStopped() || done;
     }
 
     // Called once after isFinished returns true
     protected void end() {
         System.out.println("End");
+        cvThread.interrupt();
         Robot.drivetrain.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        cvThread.interrupt();
         Robot.drivetrain.tankDrive(0.0, 0.0);
     }
 
     protected void rotate() {
-        double speed = 0.53 + 0.15 * Math.pow(howMuchWeHaveToGo(), 2); // test
-        System.out.println("Speed: " + speed);
+        double speed = 0.53 + 0.1 * Math.pow(howMuchWeHaveToGo(), 2); // test
+        System.out.println("Rotation speed: " + speed + "\nhowMuchWeHaveToGo(): " + howMuchWeHaveToGo());
         if (angle > 0) {
             System.out.println("Turning right. Angle:\t" + angle);
             Robot.drivetrain.tankDrive(speed, -speed);
@@ -115,6 +114,7 @@ public class FollowObjectCommand extends AutoMovementCommand {
         double speed = 0.7 + 0.3 * Math.min(1.0, Math.pow(inchesToMove() / distForMaxSpeed, 2)); // test
         System.out.println("Distance: " + distance);
         speed *= Math.signum(distance);
+        System.out.println("Speed: " + speed);
         Robot.drivetrain.tankDrive(speed, speed);
     }
 
@@ -137,5 +137,20 @@ public class FollowObjectCommand extends AutoMovementCommand {
             return gyro - 360;
         }
         return gyro;
+    }
+
+    private class CVThread implements Runnable {
+
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                double time = System.currentTimeMillis();
+                cvReading = Robot.vision.processImage();
+                System.out.println("\nTime: " + (System.currentTimeMillis() - time) + "\n");
+                if (cvReading != null) {
+                    angle = StuyVision.frameXPxToDegrees(cvReading[0]);
+                    distance = StuyVision.findBotDistanceToGoal(cvReading[1]);
+                }
+            }
+        }
     }
 }
