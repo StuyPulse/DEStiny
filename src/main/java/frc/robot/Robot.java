6710baa4546bcd108c45d5a44989fd2d1eab1bc6
prog694 <prog694@gmail.com>
2019-10-16 19:31:46 -0400

    Move drivetrain code to drivetrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,14 +1,9 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Gamepad.GamepadSwitchMode;
-
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import frc.robot.subsystems.Drivetrain;
 
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
@@ -16,49 +11,19 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  private WPI_TalonSRX leftFrontMotor;
-  private WPI_TalonSRX rightFrontMotor;
-  private WPI_TalonSRX leftRearMotor;
-  private WPI_TalonSRX rightRearMotor;
-  private DifferentialDrive differentialDrive;
-  private SpeedControllerGroup leftSpeedController;
-  private SpeedControllerGroup rightSpeedController;
-
-  private WPI_VictorSPX shooterMotor;
-
-  public Gamepad driverGamepad;
+  public static Drivetrain drivetrain;
   
   @Override
   public void robotInit() {
+    drivetrain = new Drivetrain();
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-
-    driverGamepad = new Gamepad(0, GamepadSwitchMode.SWITCH_X);
-    
-    leftFrontMotor = new WPI_TalonSRX(1);
-    rightFrontMotor = new WPI_TalonSRX(3);
-    leftRearMotor = new WPI_TalonSRX(4);
-    rightRearMotor = new WPI_TalonSRX(2);
-    leftFrontMotor.setInverted(true);
-    rightFrontMotor.setInverted(true);
-    leftRearMotor.setInverted(true);
-    rightRearMotor.setInverted(true);
-    leftSpeedController = new SpeedControllerGroup(leftFrontMotor, leftRearMotor);
-    rightSpeedController = new SpeedControllerGroup(rightFrontMotor, rightRearMotor);
-    differentialDrive = new DifferentialDrive(leftSpeedController, rightSpeedController);
-
-    shooterMotor = new WPI_VictorSPX(13);
-  }
-
-  @Override
-  public void robotPeriodic() {
   }
 
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
   }
 
@@ -71,15 +36,5 @@ public class Robot extends TimedRobot {
       default:
         break;
     }
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    differentialDrive.tankDrive(driverGamepad.getLeftY(), driverGamepad.getRightY());
-    shooterMotor.set(driverGamepad.getRawRightTriggerAxis());
-  }
-
-  @Override
-  public void testPeriodic() {
   }
 }
